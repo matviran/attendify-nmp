7329e6920de8c337ed76753160115ee5a0bd1038
Onkar Yaglewad <20220802065@dypiu.ac.in>
2023-03-22 20:39:42 +0530

    GUI Setup

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 
+imgBackground = cv2.imread('Resources/background.png')
+
 while True:
     success, img = cap.read()
-    cv2.imshow("Face Attendance", img)
+
+    imgBackground[162:162+480,55:55+640] = img
+
+    # cv2.imshow("Webcam", img)
+    cv2.imshow("Face Attendance", imgBackground)
     cv2.waitKey(1)
