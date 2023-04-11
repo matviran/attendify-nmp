c4968f8dcdb9ccc21030c55aee98da489da4993c
Onkar Yaglewad <20220802065@dypiu.ac.in>
2023-04-11 22:35:08 +0530

    User Data Export to DB

--- DB-export.py
+++ DB-export.py
@@ -8,3 +8,17 @@ firebase_admin.initialize_app(cred, {
 })
 
 ref = db.reference('Students')
+
+data = {
+    '20220802065':{
+        'name' : 'Onkar Yaglewad',
+        'major' : 'B Tech CSE',
+        'starting_year' : 2022,
+        'total_attendance' : 6,
+        'year' : 4,
+        'last_attendance_time' : '2023-4-10 00:54:34'
+    }
+}
+
+for key, value in data.items():
+    ref.child(key).set(value)
